47ef53892ca4bfcf0d1ef943263964eb65bcaff7
sara <sara.berling@hotmail.com>
2018-09-21 09:25:41 +0200

    Added subclass Key

---
+++ Uppgift3-Spel/Key.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift3_Spel
+{
+    public class Key : Item
+    {
+        public Key(string name, string description, int id) : base(name, description, id)
+        {
+
+        }
+    }
+}
