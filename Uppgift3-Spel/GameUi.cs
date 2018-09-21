9d249516564bdf4284439c78015badd06f5092e5
Fredrik Rönnehag <fredrik.ronnehag@gmail.com>
2018-09-21 10:54:26 +0200

    Working on GameUi switch.

--- Uppgift3-Spel/GameUi.cs
+++ Uppgift3-Spel/GameUi.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace Uppgift3_Spel
 {
@@ -26,72 +27,71 @@ namespace Uppgift3_Spel
             // om input är tom, gå tillbaks hit.
             
             var inputArray = input.Split(' ');
+            var statement = inputArray[0];
 
-            switch(inputArray[0].ToLower())
+            switch(statement.ToLower())
             {
                 case "open":
+                    Open(inputArray);
                     break;
                 case "use":
+                    Use(inputArray);
                     break;
-                case "show":
-                    Show(inputArray[1]);
+                case "show": case "inventory":
+                    Show(inputArray);
                     break;
                 case "take":
+                    Take(inputArray);
                     break;
                 case "go":
+                    Go((inputArray));
                     break;
                 case "drop":
+                    Drop((inputArray));
+                    break;
+                default:
+                    Console.WriteLine();
                     break;
             }
-            
         }
 
-        public void Show(string value)
+        public void Show(string[] value)
         {
-            if (value.ToLower() == "inventory")
+            foreach (var str in value)
             {
-                _player.ShowInventory();
+                if (str.ToLower() == "inventory")
+                {
+                    _player.ShowInventory();
+                }
             }
         }
 
-        public void Use(string value)
+        public void Use(string[] value)
         {
-            if (value.ToLower() == "use")
-            {
-
-            }
+            // ordet value
+            // kolla om spelaren har det i sitt inventory
+            // kolla om det kan användas på det andra föremålet
+            // kolla om det kan användas på exit
         }
 
-        public void Open(string value)
+        public void Open(string[] value)
         {
-            if (value.ToLower() == "open")
-            {
 
-            }
         }
 
-        public void Take(string value)
+        public void Take(string[] value)
         {
-            if (value.ToLower() == "take" || value.ToLower() == "pick up")
-            {
-
-            }
+  
         }
         
-        public void Go(string value)
+        public void Go(string[] value)
         {
-            if (value.ToLower() == "go")
-            {
 
-            }
         }
 
-        public void Drop(string value)
+        public void Drop(string[] value)
         {
-            if(value.ToUpper() == "drop")
-            {
 
-            }
         }
 
         public void PlayerParse()
