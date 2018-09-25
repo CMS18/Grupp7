08d986cdef77f2eca705fed677ac6b81945db903
sara <sara.berling@hotmail.com>
2018-09-25 16:07:56 +0200

    Skapade rum 4 och exits

--- Uppgift3-Spel/Items/Lighter.cs
+++ Uppgift3-Spel/Items/Lighter.cs
@@ -11,5 +11,16 @@ namespace Uppgift3_Spel.Items
         public Lighter(string name, string description, int id, string examine) : base(name, description, id, examine)
         {
         }
+
+        public override Item Use(Player player, Item item)
+        {
+            var findItem = player.PlayerInventory.FirstOrDefault(x => x.ItemId == 8);
+
+            if (findItem == null) return null;
+            player.PlayerInventory.Remove(findItem);
+            player.PlayerInventory.Remove(item);
+            Console.WriteLine($"{player.Name} got a Lighted Torch.");
+            return new Item("Lighted Torch", "It's a torch that is burning bright.", 10,"Woah! Keep it away from my face!");
+        }
     }
 }
