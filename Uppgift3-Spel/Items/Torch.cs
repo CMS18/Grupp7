ed632236cc6c866fcde8d58ac9b4b63d998ca5cd
Fredrik Rönnehag <fredrik.ronnehag@gmail.com>
2018-09-26 09:17:22 +0200

    Fixade broom och torch.

---
+++ Uppgift3-Spel/Items/Torch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift3_Spel.Items
+{
+    class Torch : Item
+    {
+        public Torch(string name, string description, int id, string examine) : base(name, description, id, examine)
+        {
+
+        }
+    }
+}
